4509cc2cfb0131da73d6eaaa90089315ab0f8c03
braveking12 <davidkadofficial@gmail.com>
2023-11-15 20:31:33 +0000

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,3 @@
-sts all shows contained in the database hbtn_0d_tvshows
 -- lists all rows of tables in a database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
